f4164a1fc0116c93773003fac3bb90e70ff8b1ef
alxzgl1 <a.zhigalov@gmail.com>
2022-04-25 21:06:55 +0100

    L15

--- MATLAB/acro_PCA_and_classification_SVM_ROC.m
+++ MATLAB/acro_PCA_and_classification_SVM_ROC.m
@@ -5,13 +5,13 @@ function acro_PCA_and_classification_SVM_ROC()
 
 clc;
 
+bShuffled = 0; % test
+
 aFile = 'HILIC_P'; % 'HILIC_N', HILIC_P', 'Lipids_N', 'Lipids_P'
 
 bZScore = 1; % z-score
 
-nTrainTestSplit = 1.0; 
-
-nExplainedVariance = 0.75;
+nEV = 0.75;
 
 nEqualiseSamples = 1;
 
@@ -62,7 +62,7 @@ Y_labels = pLabelsClass; % [1 x labels]
 d = diag(S);
 ev = cumsum(d) / sum(d); % explained variance
 % keep K components
-th = nExplainedVariance;
+th = nEV;
 K = find(ev > th, 1);
 V = V(:, 1:K); % eig: V = V(:, i(1:K));
 % plot eigen-values
@@ -79,85 +79,63 @@ y = Y_labels;
 %   X = (X - mean(X, 2)) ./ std(X, 0, 2);
 % end
 
-% loop shuffles
-for bShuffled = 0:1
-  % init splits
-  nSplit = nTrainTestSplit;
-  fprintf(1, 'split: %d/%d | shuffled (%d)\n', round(100 * nSplit), round(100 * (1 - nSplit)), bShuffled);
-  % init labels
-  labels_1 = find(Y_labels == 1); 
-  labels_0 = find(Y_labels == 0); 
+% init labels
+labels_1 = find(Y_labels == 1); 
+labels_0 = find(Y_labels == 0); 
 
-  % labels
-  labels_1 = labels_1(randperm(length(labels_1)));
-  labels_0 = labels_0(randperm(length(labels_0)));
+% N1 = length(labels_1); labels_1 = labels_1(1:N1);
+% N0 = length(labels_0); labels_0 = labels_0(1:N0);
 
-  N1 = length(labels_1); H1 = round(nSplit * N1);
-  N0 = length(labels_0); H0 = round(nSplit * N0); 
+N1 = length(labels_1); % H1 = N1;
+N0 = length(labels_0); % H0 = N0; 
 
-  if nEqualiseSamples == 1
-    H = min(H1, H0); H1 = H; H0 = H;
-    N = min(N1, N0); N1 = N; N0 = N;
-  end
-
-  iTR = [labels_1(1:H1), labels_0(1:H0)];
-  iTS = [labels_1((H1 + 1):N1), labels_0((H0 + 1):N0)];
-
-  % init labels
-  y_TR = y(iTR);
-  y_TS = y(iTS);
-
-  % init data
-  X_TR = X(:, iTR);
-  X_TS = X(:, iTS);
-
-  % shuffle labels
-  if bShuffled == 1
-    y_TR = y_TR(randperm(length(y_TR)));
-    y_TS = y_TS(randperm(length(y_TS)));
-  end
-
-  % z-score
-  if bZScore == 1
-    X_TR = (X_TR - mean(X_TR, 2)) ./ std(X_TR, 0, 2);
-    X_TS = (X_TS - mean(X_TS, 2)) ./ std(X_TS, 0, 2);
-  end
-
-  % init ensemble learning
-  X_train = X_TR';
-  Y_train = y_TR';
-  X_test = X_TS';
-  Y_test = y_TS';
-
-  % i_train = randperm(length(Y_train));
-  % i_test = randperm(length(Y_test));
-
-  % X_train = X_train(i_train, :);
-  % Y_train = Y_train(i_train, :);
-
-  % X_test = X_test(i_test, :);
-  % Y_test = Y_test(i_test, :);
-
-  % SVM
-  mdl = fitcsvm(X_train, Y_train, 'KernelFunction', 'linear', 'KernelScale', 'auto');
-  % compute the posterior probabilities (scores)
-  mdl = fitPosterior(mdl);
-  [~, scores] = resubPredict(mdl);
-  % compute the standard ROC curve using the scores from the SVM model.
-  % [svm_X, svm_Y, svm_T, svm_AUC] = perfcurve(Y_train, scores(:, mdl.ClassNames), 1);
-  [svm_X, svm_Y, svm_T, svm_AUC] = perfcurve(Y_train, scores(:, 2), 1);
-
-  % display the area under the curve
-  svm_AUC
-  
-  % plot the ROC curve
-  plot(svm_X, svm_Y);
-  xlabel('False positive rate');
-  ylabel('True positive rate');
-    
-  % accuracy
-  % fprintf(1, '%s: accuracy = %1.2f\n', aEnsemble, 100 * mean(y_ens == Y_test));
+if nEqualiseSamples == 1
+  N = min(N1, N0); N1 = N; N0 = N;
 end
+
+% permute labels
+% j_labels_1 = labels_1(randperm(length(labels_1)));
+% j_labels_0 = labels_0(randperm(length(labels_0)));
+% iTR = [j_labels_1(1:N1), j_labels_0(1:N0)];
+
+iTR = [labels_1(1:N1), labels_0(1:N0)];
+
+% init labels
+y_TR = y(iTR);
+% init data
+X_TR = X(:, iTR);
+
+% shuffle labels
+if bShuffled == 1
+  y_TR = y_TR(randperm(length(y_TR)));
+end
+
+% z-score
+if bZScore == 1
+  X_TR = (X_TR - mean(X_TR, 2)) ./ std(X_TR, 0, 2);
+end
+
+% SVM
+mdl = fitcsvm(X_TR', y_TR', 'KernelFunction', 'linear', 'KernelScale', 'auto');
+
+% compute the posterior probabilities (scores)
+mdl = fitPosterior(mdl);
+[~, scores] = resubPredict(mdl);
+
+% compute the standard ROC curve using the scores from the SVM model.
+% [svm_X, svm_Y, svm_T, svm_AUC] = perfcurve(y_TR', scores(:, mdl.ClassNames), 1);
+[svm_X, svm_Y, svm_T, svm_AUC] = perfcurve(y_TR', scores(:, 2), 1);
+
+% display the area under the curve
+svm_AUC
+
+% plot the ROC curve
+plot(svm_X, svm_Y);
+xlabel('False positive rate');
+ylabel('True positive rate');
+  
+% accuracy
+% fprintf(1, '%s: accuracy = %1.2f\n', aEnsemble, 100 * mean(y_ens == Y_test));
 
 end % end
 
